8980f1a671dafb800f5f211143a47f789c3b1919
alex homer <realalexhomer@gmail.com>
2019-10-31 10:28:55 -0700

    add video-upload, start adding gql queries...

--- modules/video-upload/server-ts/resolvers.ts
+++ modules/video-upload/server-ts/resolvers.ts
@@ -1,5 +1,49 @@
+// const COUNTER_SUBSCRIPTION = 'counter_subscription';
+
+// export default (pubsub: any) => ({
+//   Query: {
+//     serverCounter(obj: any, args: any, context: any) {
+//       return context.Counter.counterQuery();
+//     }
+//   },
+//   Mutation: {
+//     async addServerCounter(obj: any, { amount }: any, context: any) {
+//       await context.Counter.addCounter(amount);
+//       const counter = await context.Counter.counterQuery();
+
+//       pubsub.publish(COUNTER_SUBSCRIPTION, {
+//         counterUpdated: { amount: counter.amount }
+//       });
+
+//       return counter;
+//     }
+//   },
+//   Subscription: {
+//     counterUpdated: {
+//       subscribe: () => pubsub.asyncIterator(COUNTER_SUBSCRIPTION)
+//     }
+//   }
+// });
+
+const VIDEO_UPLOAD_SUBSCRIPTION = 'video_upload_subscription';
+
 export default (pubsub: any) => ({
   Query: {},
-  Mutation: {},
-  Subscription: {}
+  Mutation: {
+    async uploadVideoMutation(obj: any, { title }: any, context: any) {
+      await context.VideoUpload.uploadVideo(title);
+      const video = await context.Video.videoUpload();
+
+      pubsub.publish(VIDEO_UPLOAD_SUBSCRIPTION, {
+        videoUploaded: { title: video.title }
+      });
+
+      return video;
+    }
+  },
+  Subscription: {
+    videoUploaded: {
+      subscribe: () => pubsub.asyncIterator(VIDEO_UPLOAD_SUBSCRIPTION)
+    }
+  }
 });
